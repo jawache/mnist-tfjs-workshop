9e46d25b215daf63566f8bd98d94bc76a35a9102
jawache <jawache@gmail..com>
2018-11-30 16:18:42 +0000

    Completed code

--- main.js
+++ main.js
@@ -28,7 +28,61 @@ async function loadData() {
  * Create a Convolutional Neural Network
  */
 function createConvModel() {
-  // TODO
+  // Create a sequential neural network model. tf.sequential provides an API
+  // for creating "stacked" models where the output from one layer is used as
+  // the input to the next layer.
+  const model = tf.sequential();
+
+  // The first layer of the convolutional neural network plays a dual role:
+  // it is both the input layer of the neural network and a layer that performs
+  // the first convolution operation on the input. It receives the 28x28 pixels
+  // black and white images. This input layer uses 16 filters with a kernel size
+  // of 5 pixels each. It uses a simple RELU activation function which pretty
+  // much just looks like this: __/
+  model.add(
+    tf.layers.conv2d({
+      inputShape: [28, 28, 1],
+      kernelSize: 3,
+      filters: 16,
+      activation: "relu"
+    })
+  );
+
+  // After the first layer we include a MaxPooling layer. This acts as a sort of
+  // downsampling using max values in a region instead of averaging.
+  // https://www.quora.com/What-is-max-pooling-in-convolutional-neural-networks
+  model.add(tf.layers.maxPooling2d({ poolSize: 2, strides: 2 }));
+
+  // Our third layer is another convolution, this time with 32 filters.
+  model.add(
+    tf.layers.conv2d({ kernelSize: 3, filters: 32, activation: "relu" })
+  );
+
+  // Max pooling again.
+  model.add(tf.layers.maxPooling2d({ poolSize: 2, strides: 2 }));
+
+  // Add another conv2d layer.
+  model.add(
+    tf.layers.conv2d({ kernelSize: 3, filters: 32, activation: "relu" })
+  );
+
+  // Now we flatten the output from the 2D filters into a 1D vector to prepare
+  // it for input into our last layer. This is common practice when feeding
+  // higher dimensional data to a final classification output layer.
+  model.add(tf.layers.flatten({}));
+
+  model.add(tf.layers.dense({ units: 64, activation: "relu" }));
+
+  // Our last layer is a dense layer which has 10 output units, one for each
+  // output class (i.e. 0, 1, 2, 3, 4, 5, 6, 7, 8, 9). Here the classes actually
+  // represent numbers, but it's the same idea if you had classes that
+  // represented other entities like dogs and cats (two output classes: 0, 1).
+  // We use the softmax function as the activation for the output layer as it
+  // creates a probability distribution over our 10 classes so their output
+  // values sum to 1.
+  model.add(tf.layers.dense({ units: 10, activation: "softmax" }));
+
+  return model;
 }
 
 /**
@@ -36,18 +90,103 @@ function createConvModel() {
  */
 
 function createDenseModel() {
-  // TODO
+  const model = tf.sequential();
+  model.add(tf.layers.flatten({ inputShape: [IMAGE_H, IMAGE_W, 1] }));
+  model.add(tf.layers.dense({ units: 42, activation: "relu" }));
+  model.add(tf.layers.dense({ units: 10, activation: "softmax" }));
+  return model;
 }
 
 /**
  * Train the model with the training data
  */
 async function trainModel() {
-  // TODO
+  // MODEL = createDenseModel();
+  MODEL = createDenseModel();
+
+  // We are using rmsprop as our optimizer.
+  // An optimizer is an iterative method for minimizing an loss function.
+  // It tries to find the minimum of our loss function with respect to the
+  // model's weight parameters.
+  const optimizer = "rmsprop";
+
+  // We compile our model by specifying an optimizer, a loss function, and a
+  // list of metrics that we will use for model evaluation. Here we're using a
+  // categorical crossentropy loss, the standard choice for a multi-class
+  // classification problem like MNIST digits.
+  // The categorical crossentropy loss is differentiable and hence makes
+  // model training possible. But it is not amenable to easy interpretation
+  // by a human. This is why we include a "metric", namely accuracy, which is
+  // simply a measure of how many of the examples are classified correctly.
+  // This metric is not differentiable and hence cannot be used as the loss
+  // function of the model.\
+  MODEL.compile({
+    optimizer,
+    loss: "categoricalCrossentropy",
+    metrics: ["accuracy"]
+  });
+
+  const trainData = DATA.getTrainData();
+
+  // This is the total number of batches across all epochs
+  let trainBatchCount = 0;
+  const totalNumBatches =
+    Math.ceil((trainData.xs.shape[0] * (1 - VALIDATION_SPLIT)) / BATCH_SIZE) *
+    EPOCHS;
+
+  console.log("🎉 Training Start");
+  await MODEL.fit(trainData.xs, trainData.labels, {
+    batchSize: BATCH_SIZE,
+    validationSplit: VALIDATION_SPLIT, // Leave out the last 15% of the training data for validation, to monitor overfitting during training.
+    epochs: EPOCHS,
+    callbacks: {
+      onBatchEnd: async (batch, logs) => {
+        trainBatchCount++;
+        let percentComplete = (
+          (trainBatchCount / totalNumBatches) *
+          100
+        ).toFixed(1);
+        PROGRESS_UI.setProgress(percentComplete);
+        PROGRESS_UI.setStatus(`ACC ${logs.acc.toFixed(3)}`);
+        console.log(`Training... (${percentComplete}% complete)`);
+        await tf.nextFrame();
+      },
+      onEpochEnd: async (epoch, logs) => {
+        valAcc = logs.val_acc;
+        console.log(`Accuracy: ${valAcc}`);
+        PROGRESS_UI.setStatus(`*ACC ${logs.val_acc.toFixed(3)}`);
+        await tf.nextFrame();
+      }
+    }
+  });
+  console.log("🍾 Training Complete");
+
+  // Do a final test of the model with the test data, check it against data it's never seen before!
+  const testData = DATA.getTestData();
+  const testResult = MODEL.evaluate(testData.xs, testData.labels);
+  const testAccPercent = testResult[1].dataSync()[0] * 100;
+  const finalValAccPercent = valAcc * 100;
+  console.log(
+    `Final validation accuracy: ${finalValAccPercent.toFixed(1)}%; ` +
+      `Final test accuracy: ${testAccPercent.toFixed(1)}%`
+  );
 }
 
+/**
+ * Given an array passed in
+ */
 function inferModel(data) {
-  // TODO
+  let inputs = tf.tensor4d(data, [1, 28, 28, 1]);
+  // tf.tidy(() => {
+  const output = MODEL.predict(inputs);
+  const distribution = output.dataSync();
+  const axis = 1;
+  const prediction = Array.from(output.argMax(axis).dataSync())[0];
+
+  // });
+  inputs.dispose();
+  output.dispose();
+  return { prediction, distribution };
 }
 
 async function loadAndTrain() {
